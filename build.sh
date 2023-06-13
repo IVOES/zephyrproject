60a26050fffec3644f7d2965cd2e4565b90cb1f1
Mingjie Shen <shen497@purdue.edu>
2023-06-13 16:46:31 -0400

    Add timeout on compilation

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 compile() {
   APP_DIR=$1
   BOARD=$(grep ':board:' $APP_DIR/README.rst | awk '{ print $2 }')
-  west build -p always $APP_DIR -b $BOARD
+  timeout 10m west build -p always $APP_DIR -b $BOARD
 }
 
 APP_ROOT_DIR=zephyr/samples/
 APP_DIRS=$(find $APP_ROOT_DIR -type d -exec test -f '{}'/CMakeLists.txt \; -print)
 for app in $APP_DIRS; do
-  timeout 10m compile $app
+  compile $app
 done
+
+exit 0
